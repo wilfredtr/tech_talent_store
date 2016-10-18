3da418a27d4080314d4e402908de52d699971ee6
Wilfred Ruck <wilfredtr@gmail.com>
2016-10-18 09:15:38 -0400

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'all' => 'storefront#all_items'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
   resources :categories
   resources :products
 
-  root 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'storefront#all_items'
+
+  # For details on the DSL available within this file, 
+  # see http://guides.rubyonrails.org/routing.html
 end
